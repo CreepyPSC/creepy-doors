8912a36c5ecf6eb2580004a3f41dc6d6ff36f0d9
Creepy <123327228+CreepyPSC@users.noreply.github.com>
2023-03-30 20:38:50 +1000

    Update scriptdata.lua

--- A-90/scriptdata.lua
+++ A-90/scriptdata.lua
@@ -1,6 +1,5 @@
-print("data test")
-local module {
-  CurrentVersion = 2;
-  DiscordCode = "BHQD5vvNxs";
-}
-return module
+function data()
+  local scriptVersion = 2;
+  local discordCode = "BHQD5vvNxs";
+  return scriptVersion, discordCode
+end
