4d5e58a83a3d321000fdf022b2a4b6dea1a707c2
Creepy <123327228+CreepyPSC@users.noreply.github.com>
2023-03-30 19:19:05 +1000

    Create scriptdata.lua

---
+++ A-90/scriptdata.lua
@@ -0,0 +1,8 @@
+-- DOORS A-90 Script Data
+-- This script contains all the data for the A-90 Doors script.
+-- The loadstring for the script data can not be changed to prevent configurations.
+
+local module {
+  CurrentVersion = 2;
+}
+return module
