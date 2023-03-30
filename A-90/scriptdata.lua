023a7468537e102ad48a3ad110a2328502c546ae
Creepy <123327228+CreepyPSC@users.noreply.github.com>
2023-03-30 20:28:36 +1000

    Update scriptdata.lua

--- A-90/scriptdata.lua
+++ A-90/scriptdata.lua
@@ -1,8 +1,4 @@
 print("data test")
--- DOORS A-90 Script Data
--- This script contains all the data for the A-90 Doors script.
--- The loadstring for the script data can not be changed to prevent configurations.
-
 local module {
   CurrentVersion = 2;
   DiscordCode = "BHQD5vvNxs";
